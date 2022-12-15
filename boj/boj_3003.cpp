0352ad086e0a1086281e33dad0de78da57fd0e38
Hong Sunghwi <tnfy10@gmail.com>
2022-12-15 23:54:57 +0900

    boj 3003

---
+++ boj/boj_3003.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int king, queen, rook, bishop, knight, pawn;
+    cin >> king >> queen >> rook >> bishop >> knight >> pawn;
+    cout << 1-king << " " << 1-queen << " " << 2-rook << " " << 2-bishop << " " << 2-knight << " " << 8-pawn << endl;
+    return 0;
+}
