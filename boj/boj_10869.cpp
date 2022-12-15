11e0fa6e38c7b3f584707b85710319159b8db385
Hong Sunghwi <tnfy10@gmail.com>
2022-12-15 23:34:55 +0900

    boj 10869

---
+++ boj/boj_10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+    return 0;
+}
