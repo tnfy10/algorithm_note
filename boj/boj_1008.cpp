0a43f87320c9c93a65050e6727f214b282659267
Hong Sunghwi <tnfy10@gmail.com>
2022-12-15 23:31:38 +0900

    boj 1008

---
+++ boj/boj_1008.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout.precision(15);
+    cout << a/b << endl;
+    return 0;
+}
