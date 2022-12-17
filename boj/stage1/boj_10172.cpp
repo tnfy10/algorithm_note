089308bd99b402f3c26d22ea56b35bf7949622a8
Hong Sunghwi <tnfy10@gmail.com>
2022-12-17 12:17:05 +0900

    boj 10172

---
+++ boj/stage1/boj_10172.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+    return 0;
+}
